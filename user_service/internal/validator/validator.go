f42e4057590f180b7a4a899efa00f318a35a9699
dmitri <dmitriklsv@gmail.com>
2023-02-27 16:53:51 +0600

    added email regep

--- user_service/internal/validator/validator.go
+++ user_service/internal/validator/validator.go
@@ -19,7 +19,7 @@ func NewValidator(cfg *configs.Configs) *Validator {
 	return &Validator{
 		passwordMin: cfg.Validator.PasswordMin,
 		passwordMax: cfg.Validator.PasswordMax,
-
+		emailReg:    regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`),
 		usernameMin: cfg.Validator.UsernameMin,
 		usernameMax: cfg.Validator.UsernameMax,
 	}
