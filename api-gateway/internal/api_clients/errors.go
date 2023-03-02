4f33f2505138ff2a6d72db7d29eaafabdbc62acc
dmitri <dmitriklsv@gmail.com>
2023-03-02 13:00:54 +0600

    added grpc code mappimh to http

---
+++ api-gateway/internal/api_clients/errors.go
@@ -0,0 +1,46 @@
+package apiclients
+
+import (
+	"net/http"
+
+	"google.golang.org/grpc/codes"
+)
+
+func gRPCToHTTP(code codes.Code) int {
+	switch code {
+	case codes.OK:
+		return http.StatusOK
+	case codes.Canceled:
+		return 499
+	case codes.InvalidArgument:
+		return http.StatusBadRequest
+	case codes.DeadlineExceeded:
+		return http.StatusGatewayTimeout
+	case codes.NotFound:
+		return http.StatusNotFound
+	case codes.AlreadyExists:
+		return http.StatusConflict
+	case codes.PermissionDenied:
+		return http.StatusForbidden
+	case codes.Unauthenticated:
+		return http.StatusUnauthorized
+	case codes.ResourceExhausted:
+		return http.StatusTooManyRequests
+	case codes.FailedPrecondition:
+		return http.StatusBadRequest
+	case codes.Aborted:
+		return http.StatusConflict
+	case codes.OutOfRange:
+		return http.StatusBadRequest
+	case codes.Unimplemented:
+		return http.StatusNotImplemented
+	case codes.Internal:
+		return http.StatusInternalServerError
+	case codes.Unavailable:
+		return http.StatusServiceUnavailable
+	case codes.DataLoss:
+		return http.StatusInternalServerError
+	default:
+		return -1
+	}
+}
--- api-gateway/internal/middlwares/error_middleware.go
+++ api-gateway/internal/middlwares/error_middleware.go
@@ -1,21 +1,22 @@
 package middlwares
 
 import (
-	"errors"
+	"fmt"
 	"net/http"
 
-	"github.com/Levap123/user_service/internal/domain"
-
-	"github.com/Levap123/utils/apperror"
+	"google.golang.org/grpc/status"
 )
 
 func CheckErrorMiddlware(prev func(w http.ResponseWriter, r *http.Request) error) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		err := prev(w, r)
 		if err != nil {
-			if errors.Is(err, domain.ErrPasswordLengthIncorrect) {
-				appErr := apperror.MakeBadRequestErr(domain.ErrPasswordLengthIncorrect, "password length should be from 8 to 20")
+			st, ok := status.FromError(err)
+			if !ok {
+				fmt.Println("123")
+				fmt.Println(err)
 			}
+			fmt.Println(st.Code(), st.Message())
 		}
 	})
 }
