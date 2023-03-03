b7d6388b48e2b37c56e59215ee4a3d7fe167681d
dmitri <dmitriklsv@gmail.com>
2023-03-03 11:47:06 +0600

    added auth middleware

---
+++ api-gateway/internal/handler/middleware.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/Levap123/api_gateway/pkg/json"
+	"github.com/Levap123/utils/apperror"
+)
+
+func (h *Handler) UserIdentity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		h.log.Debug("refresh")
+
+		authHeader := r.Header.Get("Authorization")
+		authHeaderSplit := strings.Split(authHeader, "Bearer ")
+		if len(authHeaderSplit) != 2 {
+			err := apperror.NewError(errors.New("auth header must be: Bearer <token>"), "invalid auth header", http.StatusUnauthorized)
+			bytes := json.Marshal(err)
+			json.SendJSON(w, bytes, http.StatusUnauthorized)
+			return
+		}
+
+		authToken := authHeaderSplit[1]
+
+		ctx, cancel := context.WithTimeout(r.Context(), time.Second*1)
+		defer cancel()
+
+		userID, err := h.userClient.Validate(ctx, authToken)
+		if err != nil {
+			err := apperror.NewError(err, "error in validating token", http.StatusUnauthorized)
+			bytes := json.Marshal(err)
+			json.SendJSON(w, bytes, http.StatusUnauthorized)
+			return
+		}
+
+		ctxWithValue := context.WithValue(r.Context(), "user_id", userID)
+
+		next.ServeHTTP(w, r.WithContext(ctxWithValue))
+	})
+}
