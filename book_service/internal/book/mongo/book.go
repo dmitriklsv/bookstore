39ac3b095cd2e2e76e90ac06042df4053ab57875
dmitri <dmitriklsv@gmail.com>
2023-03-03 14:43:55 +0600

    added proto file

--- book_service/internal/book/mongo/book.go
+++ book_service/internal/book/mongo/book.go
@@ -1,6 +1,13 @@
 package mongo
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"fmt"
+
+	"github.com/Levap123/book_service/internal/book"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type BookRepo struct {
 	coll *mongo.Collection
@@ -10,4 +17,14 @@ func NewBookRepo(DB *mongo.Client) *BookRepo {
 	return &BookRepo{
 		coll: DB.Database("bookstore").Collection("books"),
 	}
+}
+
+func (br *BookRepo) Create(ctx context.Context, book *book.Book) (string, error) {
+	res, err := br.coll.InsertOne(ctx, book)
+	if err != nil {
+		return "", fmt.Errorf("book repo - create - %w", err)
+	}
+
+	ID := res.InsertedID.(primitive.ObjectID).Hex()
+	return ID, nil
 }
