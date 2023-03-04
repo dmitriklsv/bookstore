b8bccc961b9e54bf7dcd850b139b3b6cfd865cea
dmitri <dmitriklsv@gmail.com>
2023-03-04 20:18:11 +0600

    added dto and model for bok

---
+++ api_gateway/internal/dto/book.go
@@ -0,0 +1,15 @@
+package dto
+
+type CreateBookDTO struct {
+	ID          string `json:"id,omitempty"`
+	Title       string `json:"title,omitempty"`
+	Description string `json:"description,omitempty"`
+	Image       string `json:"image,omitempty"`
+	Pages       uint64 `json:"pages,omitempty"`
+	Author      string `json:"author,omitempty"`
+	Genre       string `json:"genre,omitempty"`
+	Publisher   string `json:"publisher,omitempty"`
+	Binding     bool   `json:"binding,omitempty"`
+	Series      string `json:"series,omitempty"`
+	Language    string `json:"language,omitempty"`
+}
